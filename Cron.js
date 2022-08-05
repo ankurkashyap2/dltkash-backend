1b9f88d1dfec47b22061d00e6c442863f5a9e659
rajukumar52 <raju@webmobsoft.com>
2022-08-05 15:48:01 +0530

    crone timeing change

--- Cron.js
+++ Cron.js
@@ -3,20 +3,20 @@ const { UCC_REQUEST_TYPES } = require('./constants');
 const cronServices = require('./services/cronServices');
 
 
-// CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED
-cron.schedule('*/5 * * * *', () => {
-    try {
-        console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED =>", (new Date()).toUTCString());
-        cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.MODIFIED).then(() => {
+// // CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED
+// cron.schedule('*/5 * * * *', () => {
+//     try {
+//         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED =>", (new Date()).toUTCString());
+//         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.MODIFIED).then(() => {
 
-        });
-    } catch (err) {
-        console.log(err)
-    }
-});
+//         });
+//     } catch (err) {
+//         console.log(err)
+//     }
+// });
 
-// CRON RUN EVERYDAY AT 11AM FOR REQUEST_TYPE>>>>>>>>NEW
-cron.schedule('*/5 * * * *', () => {
+// // CRON RUN EVERYDAY AT 11AM FOR REQUEST_TYPE>>>>>>>>NEW
+cron.schedule('*/2 * * * *', () => {
     try {
         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>NEW =>", (new Date()).toUTCString());
         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.NEW).then(() => {
@@ -27,13 +27,13 @@ cron.schedule('*/5 * * * *', () => {
     }
 });
 
-// CRON RUN EVERYDAY AT 7PM FOR REQUEST_TYPE>>>>>>>>EXISTING
-cron.schedule('*/5 * * * *', () => {
-    try {
-        console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>EXISTING =>", (new Date()).toUTCString());
-        cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.EXISTING).then(() => {
-        });
-    } catch (err) {
-        console.log(err)
-    }
-});
+// // CRON RUN EVERYDAY AT 7PM FOR REQUEST_TYPE>>>>>>>>EXISTING
+// cron.schedule('*/5 * * * *', () => {
+//     try {
+//         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>EXISTING =>", (new Date()).toUTCString());
+//         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.EXISTING).then(() => {
+//         });
+//     } catch (err) {
+//         console.log(err)
+//     }
+// });
