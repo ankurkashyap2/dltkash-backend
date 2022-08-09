5c2f9ef698e791d833c387b940a1920fb5c77565
rajukumar52 <raju@webmobsoft.com>
2022-08-09 15:34:40 +0530

    crone time change to 2 minute

--- Cron.js
+++ Cron.js
@@ -4,19 +4,19 @@ const cronServices = require('./services/cronServices');
 
 
 // // CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED
-// cron.schedule('*/5 * * * *', () => {
-//     try {
-//         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED =>", (new Date()).toUTCString());
-//         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.MODIFIED).then(() => {
+cron.schedule('*/3 * * * *', () => {
+    try {
+        console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>MODIFIED =>", (new Date()).toUTCString());
+        cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.MODIFIED).then(() => {
 
-//         });
-//     } catch (err) {
-//         console.log(err)
-//     }
-// });
+        });
+    } catch (err) {
+        console.log(err)
+    }
+});
 
 // // CRON RUN EVERYDAY AT 11AM FOR REQUEST_TYPE>>>>>>>>NEW
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('*/3 * * * *', () => {
     try {
         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>NEW =>", (new Date()).toUTCString());
         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.NEW).then(() => {
@@ -28,12 +28,12 @@ cron.schedule('*/2 * * * *', () => {
 });
 
 // // CRON RUN EVERYDAY AT 7PM FOR REQUEST_TYPE>>>>>>>>EXISTING
-// cron.schedule('*/5 * * * *', () => {
-//     try {
-//         console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>EXISTING =>", (new Date()).toUTCString());
-//         cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.EXISTING).then(() => {
-//         });
-//     } catch (err) {
-//         console.log(err)
-//     }
-// });
+cron.schedule('*/3 * * * *', () => {
+    try {
+        console.info("CRON RUN EVERYDAY AT 4:30PM FOR REQUEST_TYPE>>>>>>>>EXISTING =>", (new Date()).toUTCString());
+        cronServices.notificationSendingLogic(UCC_REQUEST_TYPES.EXISTING).then(() => {
+        });
+    } catch (err) {
+        console.log(err)
+    }
+});
